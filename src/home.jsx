06250dd7c14b8f7246631f36e50684358e8a9901
yangkun <yangkun@leying365.com>
2021-11-30 18:24:32 +0800

    change config

--- src/home.jsx
+++ src/home.jsx
@@ -1,124 +1,152 @@
-import React from "react";
-// import { Layout, Calendar } from "antd";
-// import Menu from "./components/Menu";
-// import Header from "./components/Header";
-
-// const { Content } = Layout;
-// import "./index.css";
-
-// class SiderDemo extends React.Component {
-//   state = {
-//     collapsed: false,
-//   };
-
-//   toggle = () => {
-//     this.setState({
-//       collapsed: !this.state.collapsed,
-//     });
-//   };
-//   onPanelChange = (value, mode) => {
-//     console.log(value.format("YYYY-MM-DD"), mode);
-//   };
-
-//   render() {
-//     return (
-//       <Layout>
-//         <Menu collapsed={this.state.collapsed} />
-//         <Layout className="site-layout">
-//           <Header toggle={this.toggle} />
-//           <Content
-//             className="site-layout-background"
-//             style={{
-//               margin: "24px 16px",
-//               padding: 24,
-//               minHeight: 280,
-//             }}
-//           >
-//             {/* <Calendar onPanelChange={this.onPanelChange} /> */}
-//             {this.props.children}
-//           </Content>
-//         </Layout>
-//       </Layout>
-//     );
-//   }
-// }
-
-// export default SiderDemo;
-
-
-
-
-
-
-
-
-
-
-import { Layout, Menu, Breadcrumb } from 'antd';
+import React, { useState,useEffect } from "react";
+import { Layout, Menu, Breadcrumb } from "antd";
 import {
-  DesktopOutlined,
-  PieChartOutlined,
   FileOutlined,
   TeamOutlined,
   UserOutlined,
-} from '@ant-design/icons';
-
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  PieChartOutlined,
+  DesktopOutlined,
+} from "@ant-design/icons";
+import {
+  Link
+} from "react-router-dom";
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
+const rootSubmenuKeys = ["1"];
+// const rootSubmenuKeys = ["sub1", "sub2"];
 
-class SiderDemo extends React.Component {
+
+const SiderMenu = (props) => {
+  const [openKeys, setOpenKeys] = useState([]);
+  const [current, setCurrent] = useState('1');
+
+
+  useEffect(() => { 
+    console.log(2222);
+    const curMenu = localStorage.getItem('curMenu');
+    const currentKey = localStorage.getItem('current');
+    if (curMenu) {
+      setOpenKeys(curMenu);
+    }
+    setCurrent(currentKey)
+  },[])
+  const onOpenChange = (keys) => {
+    const latestOpenKey = keys.find((key) => openKeys.indexOf(key) === -1);
+    if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+      setOpenKeys(keys);
+    } else {
+      setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+    }
+   
+    console.log(keys);
+  };
+  const handleClick = e => {
+    console.log('click ', e);
+    setCurrent(e.key)
+    console.log(current);
+    if (e.keyPath.length>=2) {
+      localStorage.setItem('curMenu',e.keyPath[e.keyPath.length-1]);
+    } else {
+      localStorage.setItem('curMenu','');
+    }
+    localStorage.setItem('current',e.key);
+    // this.setState({
+    //   current: e.key,
+    // });
+  };
+
+  
+
+  return (
+    
+    <Menu
+      theme="dark"
+      mode="inline"
+      openKeys={openKeys}
+      onClick={handleClick}
+      onOpenChange={onOpenChange}
+      selectedKeys={[current]}
+    >
+      {/* defaultSelectedKeys={["sub2"]} */}
+
+      <Menu.Item key="1" icon={<PieChartOutlined />}>
+        
+        <Link to="/index">Option 1</Link>
+      </Menu.Item>
+      <Menu.Item key="2" icon={<DesktopOutlined />}>
+        Option 2
+      </Menu.Item>
+      <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+        <Menu.Item key="3">Tom</Menu.Item>
+        <Menu.Item key="4">Bill</Menu.Item>
+        <Menu.Item key="5">Alex</Menu.Item>
+      </SubMenu>
+      <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+        <Menu.Item key="6">Team 1</Menu.Item>
+        <Menu.Item key="8">Team 2</Menu.Item>
+      </SubMenu>
+      <Menu.Item key="9" icon={<FileOutlined />}>
+        Files
+      </Menu.Item>
+    </Menu>
+  );
+};
+class Home extends React.Component {
   state = {
     collapsed: false,
   };
-
-  onCollapse = collapsed => {
-    console.log(collapsed);
-    this.setState({ collapsed });
+  toggleCollapsed = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
   };
-
+ 
+  // componentWillMount() {
+  //   console.log(1111);
+  // }
   render() {
     const { collapsed } = this.state;
     return (
-      <Layout style={{ minHeight: '100vh' }}>
-        <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
+          trigger={null}
+          collapsible
+          onCollapse={() => this.toggleCollapsed()}
+          collapsed={this.state.collapsed}
+        >
           <div className="logo" />
-          <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <Menu.Item key="1" icon={<PieChartOutlined />}>
-              Option 1
-            </Menu.Item>
-            <Menu.Item key="2" icon={<DesktopOutlined />}>
-              Option 2
-            </Menu.Item>
-            <SubMenu key="sub1" icon={<UserOutlined />} title="User">
-              <Menu.Item key="3">Tom</Menu.Item>
-              <Menu.Item key="4">Bill</Menu.Item>
-              <Menu.Item key="5">Alex</Menu.Item>
-            </SubMenu>
-            <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
-              <Menu.Item key="6">Team 1</Menu.Item>
-              <Menu.Item key="8">Team 2</Menu.Item>
-            </SubMenu>
-            <Menu.Item key="9" icon={<FileOutlined />}>
-              Files
-            </Menu.Item>
-          </Menu>
+          <SiderMenu />
         </Sider>
         <Layout className="site-layout">
-          <Header className="site-layout-background" style={{ padding: 0 }} />
-          <Content style={{ margin: '0 16px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            {React.createElement(
+              collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+              {
+                className: "trigger",
+                onClick: this.toggleCollapsed,
+              }
+            )}
+          </Header>
+          <Content style={{ margin: "0 16px" }}>
+            <Breadcrumb style={{ margin: "16px 0" }}>
               <Breadcrumb.Item>User</Breadcrumb.Item>
               <Breadcrumb.Item>Bill</Breadcrumb.Item>
             </Breadcrumb>
-            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+            <div
+              className="site-layout-background"
+              style={{ padding: 24, minHeight: 360 }}
+            >
               Bill is a cat.
             </div>
           </Content>
-          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+          <Footer style={{ textAlign: "center" }}>
+            Ant Design ©2018 Created by Ant UED
+          </Footer>
         </Layout>
       </Layout>
     );
   }
 }
-export default SiderDemo;
-// ReactDOM.render(<SiderDemo />, mountNode);
+export default Home;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,41 @@
+
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-
+import {resolve} from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [reactRefresh()],
+  // 部署应用包时的基本 URL,用法和 webpack 本身的 output.publicPath 一致
+  // base: "./",
+  resolve: {
+    alias: {
+      // 键必须以斜线开始和结束
+      "@": resolve(__dirname, "src"),
+      img: resolve(__dirname, "src/assest/images"),
+      views: resolve(__dirname, "src/views"),
+      // "components": resolve(__dirname, "src/components"),
+      // "utils": resolve(__dirname, "src/utils"),
+    },
+  },
+  // css: {
+  //   // 🔥此处添加全局scss🔥
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: '@import "./src/variables.scss";'
+  //     }
+  //   }
+  // },
+  server: {
+    open: true,
+    host: "localhost",
+    // port: 8090,
+    https: false,
+    // http 代理配置
+    proxy: {
+      // "/api": {
+      //   target: "http://192.168.10.131:8899",
+      // changeOrigin: true,
+      //   rewrite: (path) => path.replace(/^\/api/, '')
+    },
+  },
+});
